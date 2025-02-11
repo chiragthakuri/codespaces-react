03cef5c3ac04c289f7172b7e7f82df8a5f47d2c9
Chirag <chiragbikram@gmail.com>
2025-02-11 23:35:36 +0000

    react basics

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,22 @@
 import './App.css';
 
+function MyButton(){
+  function handleClick(){
+    alert('You clicked me!');
+  }
+  return(
+    <button onClick={handleClick}>
+      Im a button
+    </button>
+  );
+}
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src="Octocat.png" className="App-logo" alt="logo" />
-        <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
-        </p>
-        <p className="small">
-          Edit <code>src/App.jsx</code> and save to reload.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
-      </header>
+      <h1>Testing Codespaces</h1>
+      <MyButton />
+      
     </div>
   );
 }
